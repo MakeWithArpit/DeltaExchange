335b2c9e0c912d4175d025923068bb5074b58e9e
Arpit <arpit.gangwar061@gmail.com>
2026-02-17 13:06:02 +0530

    push

--- delta.py
+++ delta.py
@@ -138,12 +138,12 @@ if __name__ == "__main__":
     # place_market_order(product_id, SIDE, SIZE))
     show_live_price(SYMBOL)
 
-    place_limit_with_stoploss(
-        product_id=product_id,
-        side="sell",           # entry side
-        size=1,
-        limit_price=68045.8,    # entry price
-        stop_price=67900      # stoploss price
-    )
+    # place_limit_with_stoploss(
+    #     product_id=product_id,
+    #     side="sell",           # entry side
+    #     size=1,
+    #     limit_price=68045.8,    # entry price
+    #     stop_price=67900      # stoploss price
+    # )
 
    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ # place_limit_with_stoploss(
+    #     product_id=product_id,
+    #     side="sell",           # entry side
+    #     size=1,
+    #     limit_price=68045.8,    # entry price
+    #     stop_price=67900      # stoploss price
+    # )
